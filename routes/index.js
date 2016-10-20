98c59b420f39f2956d9ae125cfdbd0427fe39193
TroyBug <sldtkx@163.com>
2016-10-21 00:35:48 +0800

    Add files via upload
    update comments and syntax.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,17 @@ var router = express.Router();
 var fs = require('fs');
 var http = require('http');
 var fileupload = require('fileupload');
-//确保系统中安装了graphicsmagick
+//graphicsmagick、ImageMagick required
+//they are stand-alone programs,not a npm module
+//download link:http://www.graphicsmagick.org/
+//http://www.imagemagick.org/
+//maybe need to restart after install if you work in windows. 
 var gm = require('gm');
-//var im = require('imagemagick');
-//var imageMagick = gm.subClass({ imageMagick: true });
+
+//use multer
+var multer = require('multer');
+var upload = multer({ dest: './public/images'});
+
 //保存上传图片路径
 var globalPath = '';
 
@@ -19,12 +26,14 @@ router.get('/upload',function(req,res) {
 	res.render('upload',{title:'图片上传'});
 });
 
-router.post('/upload',function(req,res) {
-	if(!req.files.uploadBtn) {
+//uploadBtn is the name of input[file] in upload.ejs
+router.post('/upload', upload.single('uploadBtn'), function(req,res) {
+	if(!req.file.originalname) {
 		res.send('<script>window.parent.handleError("请选择上传文件");history.go(-1);</script>');
 	}
-	var path = req.files['uploadBtn'].path;
-    //public\images\798c224275d707cdb6c80acbf234e744.jpg
+	//var path = req.file['uploadBtn'].path;
+    var path = req.file['path'];
+    //path would be like this:  public\images\798c224275d707cdb6c80acbf234e744.jpg
 	path = path.replace(/\\/g,'/').replace('public','');
     globalPath = path;
 
@@ -38,7 +47,8 @@ router.post('/save',function(req,res) {
         preImgSize = req.body.preSize.split(',');   //预览图当前宽高
 
 
-    gm('public'+globalPath).resize(preImgSize[0],preImgSize[1]).crop(150,150,x,y).write('public/images/crop.jpg',function(err) {
+    gm('public'+globalPath).resize(preImgSize[0],preImgSize[1]).crop(150,150,x,y)
+        .write('public/images/crop.jpg',function(err) {
         if(err) throw err;
         res.render('index',{title:'js图像裁剪并上传Demo'});
     });
